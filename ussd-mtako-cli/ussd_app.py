65198aa48687dbb4c6cd84ab2a058f85f0b134cc
ifilipechau <filipechau.dev@gmail.com>
2025-07-22 17:51:13 +0200

    instanciar o sistema e usar o loop while para mostrar o menu principal e possibilitar a escolha de servicos

---
+++ ussd-mtako-cli/ussd_app.py
@@ -0,0 +1,25 @@
+# ussd_app.py
+
+# Importar funções e classes necessárias para o sistema
+
+from menu import main_menu, get_service_menu
+from core import Mtako
+
+def run_ussd():
+    """
+    Função principal que executa o loop da aplicação USSD na linha de comandos.
+    """
+    mtako = Mtako() # Instancia o sistema
+
+    while True:
+        # Mostra o menu principal e espera a escolha do utilizador
+        opcao = input(main_menu())
+
+        # Encerrar a sessão
+        if opcao == "0":
+            print("Sessão terminada. Obrigado por usar M-Tako CLI.")
+            break
+
+        # Enviar Dinheiro
+        elif opcao == "1":
+            print(get_service_menu)
