1b312965b0d80ae1c9594badf9ed19cd30c7e062
ifilipechau <filipechau.dev@gmail.com>
2025-07-22 16:42:03 +0200

    add: adicionar a funcao main_menu para retornar o menu principal do sistema mTako

---
+++ ussd-mtako-cli/menu.py
@@ -0,0 +1,16 @@
+# menu.py
+
+def main_menu():
+    """
+    Retorna o menu principal do sistema USSD.
+    """
+    return """
+
+Bem-vindo ao mTako CLI
+1. Enviar Dinheiro
+2. Levantar Dinheiro
+3. Pagamento de Serviço
+4. Consultar Saldo
+5. Alterar PIN
+0. Sair
+Escolha uma opção: """
